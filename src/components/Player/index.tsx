3f2908b122e7eb5d8c91928168c7b0a197a21e57
pallomagualter <pallomagualter@gmail.com>
2021-04-23 15:18:43 -0300

    player

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -12,7 +12,38 @@ export function Player() {
 				<strong>Selecione um podcast para ouvir</strong>
 			</div>
 
-			<footer></footer>
+			<footer className={styles.empty}>
+				<div className={styles.progress}>
+						<span>00:00</span>
+						<div className={styles.slider}>
+							<div className={styles.emptySlider} />
+						</div>
+						<span>00:00</span>
+				</div>
+
+					<div className={ styles.buttons}>
+						<button type="button" >
+							<img src="/shuffle.svg" alt="Embaralhar"/>
+						</button>
+
+						<button type="button" >
+							<img src="/play-previous.svg" alt="tocar anterior"/>
+						</button>
+
+						<button type="button" className={styles.playButton}>
+							<img src="/play.svg" alt="Tocar"/>
+						</button>
+
+						<button type="button" >
+							<img src="/play-next.svg" alt="tocar próxima"/>
+						</button>
+
+						<button type="button" >
+							<img src="/repeat.svg" alt="Repetir"/>
+						</button>
+						
+					</div>
+			</footer>
 		</div>
 	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,5 +3,6 @@ import { Header } from "../components/Header";
 export default function Home() {
   return (
     <Header />
-  )
+  );
+  
 }
