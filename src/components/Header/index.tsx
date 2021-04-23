b5f43a66c43fd8b3449d934b34710033c48b7bf7
pallomagualter <pallomagualter@gmail.com>
2021-04-23 12:13:48 -0300

    component header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,8 @@
+import styles from './styles.module.scss';
+
 export function Header() {
     return (
-        <header>
+        <header className={styles.headerContainer}>
             <img src="/logo.svg" alt="Podcastr"/>
 
             <p>O melhor para você ouvir, sempre</p>
