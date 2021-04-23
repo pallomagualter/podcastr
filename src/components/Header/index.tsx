6fa62bd27ecc6ba97c21aa6dd2049b0bf01df7e9
pallomagualter <pallomagualter@gmail.com>
2021-04-23 12:30:53 -0300

    component Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,21 @@
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import styles from './styles.module.scss';
 
 export function Header() {
+    const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+        locale: ptBR,
+    }); 
+    //para retornar primeira 3 letras dia semana, mais dia e mês completo
+
     return (
         <header className={styles.headerContainer}>
             <img src="/logo.svg" alt="Podcastr"/>
 
             <p>O melhor para você ouvir, sempre</p>
 
-            <span>Qui, 8 Abril</span>
+            <span>{currentDate}</span>
         </header>
     );
 }
